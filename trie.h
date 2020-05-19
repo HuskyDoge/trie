83264ff999c9c8dc5ea309e7cf307f35b2ba7bf7
Jialun Li <cslijialun@126.com>
2020-05-19 13:22:43 +0800

    Add files via upload
    update

--- trie.h
+++ trie.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include <stdexcept>
+#include <functional>
 
 namespace husky {
     const int KEY_SIZE = 'z' - 'a' + 1 + 10; // alphabet + number
@@ -9,6 +11,12 @@ namespace husky {
     public:
         trie_node *children[KEY_SIZE];
         T data;
+
+        trie_node& operator=(trie_node& other) {
+            this->children = other.children;
+            this->data = other.data;
+            return *this;
+        }
 
         trie_node() {
             memset(children, 0, sizeof(children));
@@ -24,6 +32,7 @@ namespace husky {
         }
     };
 
+
     template<typename T>
     class trie {
     public:
@@ -31,9 +40,22 @@ namespace husky {
             root = new trie_node<T>();
         }
 
+        trie& operator=(trie& other) {
+            this->keys = other.keys;
+            this->root = other.root;
+            return *this;
+        }
+
+        trie(const trie& other) {
+            this->keys = other.keys;
+            this->root = other.root;
+        }
+
         ~trie() {
-            delete root;
-            root = nullptr;
+            if (root != nullptr) {
+                delete root;
+                root = nullptr;
+            }
         }
 
         // convert char to the corresponding index
@@ -57,6 +79,7 @@ namespace husky {
                 current = current->children[curidx];
             }
             current->data = t;
+            keys.push_back(s);
         }
 
         T& search(const char *s, int N) {
@@ -64,12 +87,76 @@ namespace husky {
             trie_node<T> *current = root;
             for (int i = 0; i < N; ++i) {
                 current = current->children[char2idx(s[i])];
-                if (current == nullptr) throw std::runtime_error("cannot find key in trie");
+                if (current == nullptr) throw std::runtime_error("cannot find" + std::string(s) + "in trie");
             }
             return current->data;
         }
 
+        T& search(const char *s) {
+            trie_node<T> *current = root;
+            int i = 0;
+            while (s[i] != '\0') {
+                current = current->children[char2idx(s[i])];
+                if (current == nullptr) throw std::runtime_error("cannot find" + std::string(s) + "in trie");
+                ++i;
+            }
+            return current->data;
+        }
+
+        T* try_search(const char *s) {
+            trie_node<T> *current = root;
+            int i = 0;
+            while (s[i] != '\0') {
+                current = current->children[char2idx(s[i])];
+                if (current == nullptr) return nullptr;
+                ++i;
+            }
+            return &current->data;
+        }
+
+
+        T* try_search(const char *s, int N) {
+            trie_node<T> *current = root;
+            for (int i = 0; i < N; ++i) {
+                current = current->children[char2idx(s[i])];
+                if (current == nullptr) return nullptr;
+            }
+            return &current->data;
+        }
+
+        void batch_process(std::function<void(T&)> func) {
+            for (const auto& key: keys) {
+                func(this->search(key.c_str(), key.size()));
+            }
+        }
+
+        T& operator[](int index) {
+            return this->search(keys[index].c_str(), keys[index].size());
+        }
+
+        bool contains(const char *s, int N) {
+            if (N == 0) throw std::runtime_error("trie N cannot be zero");
+            trie_node<T> *current = root;
+            for (int i = 0; i < N; ++i) {
+                current = current->children[char2idx(s[i])];
+                if (current == nullptr) return false;
+            }
+            return true;
+        }
+
+        const std::vector<std::string>& get_keys() {
+            return keys;
+        }
+
+        void clear() {
+            delete root;
+            root = nullptr;
+            keys.clear();
+            root = new trie_node<T>();
+        }
+
     private:
         trie_node<T> *root;
+        std::vector<std::string> keys;
     };
 }
